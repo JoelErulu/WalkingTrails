de829d3c6406aaa78fb2074a0540cde94b7dc0a1
Charlie <71472662+LoveTofuDGAF@users.noreply.github.com>
2022-10-05 11:44:52 -0400

    Update index.js
    added connection url for mongo

--- TrailApp/server/index.js
+++ TrailApp/server/index.js
@@ -19,6 +19,6 @@ app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL || mongodb+srv://admin:admin@trailcluster.txlu38n.mongodb.net/?retryWrites=true&w=majority, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
