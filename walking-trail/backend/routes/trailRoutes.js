4e0996f4f62b19ea8d37d7719affd6814a48717b
serenabonsu1 <101145198+serenabonsu1@users.noreply.github.com>
2022-11-16 14:11:12 -0500

    Delete walking-trail directory

--- walking-trail/backend/routes/trailRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getTrails, setTrails, updateTrails, deleteTrails} = require('../controllers/trailController')
-
-router.route('/').get(getTrails).post(setTrails)
-router.route('/:id').delete(deleteTrails).put(updateTrails)
-
-
-module.exports = router
