6c92e5ba3883d00699678fa18c307bef8905d9c8
Daniel Valencia <dvalencia2@ggc.edu>
2023-04-25 17:21:25 -0400

    adding to nutrition page

--- TrailApp/client/src/components/Nutrition/Nutrition.js
+++ TrailApp/client/src/components/Nutrition/Nutrition.js
@@ -2,12 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Grid, Typography, Container, Button, Paper, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 import { useDispatch, useSelector} from 'react-redux';
-import { getTrails } from '../../actions/trails.js';
 import useStyles from './styles.js';
-import gold from '../../images/gold.png';
-import green from '../../images/green.png';
-import gray from '../../images/gray.png';
-
+import { createMarker, getMarkers, updateMarker, deleteMarker} from '../../actions/markers.js';
+import FileBase from 'react-file-base64';
 
 const Nutrition = () => {
 
@@ -19,8 +16,23 @@ const Nutrition = () => {
         <Container component="main" maxWidth="xl">
         
 
-                <Paper className={classes.paper} elevation={3}>
-                    
+                {/* <Paper className={classes.paper} elevation={3}>
+                <form onSubmit={handleSubmit}>
+                <TextField name='name' variant="outlined" label="Name" margin="normal" value={markerFormData.name}
+                onChange={(e) => setMarkerFormData({...markerFormData, name: e.target.value})}></TextField>
+                <br/>
+                <TextField name='exersice' variant="outlined" label="Exercise" margin="normal" value={markerFormData.exercise}
+                onChange={(e) => setMarkerFormData({...markerFormData, exercise: e.target.value})}></TextField>
+                <br/>
+                <Collapse>
+                <TextField name='lat' variant="outlined" label="Latitude" value = {markerFormData.lat} InputLabelProps={{ shrink: true }} margin="normal"></TextField>
+                <br/>
+                <TextField name='lng' variant="outlined" label="Longitude" value = {markerFormData.lng} InputLabelProps={{ shrink: true }} margin="normal"></TextField>
+                </Collapse>
+                <br/>
+                <div><FileBase type="file" multiple={false} onDone={({ base64 }) => setMarkerFormData({ ...markerFormData, img: base64 })}/></div>
+                <Button type='submit' color="primary" variant="contained">Create</Button>
+                </form> */}
                     <br/>
                     <br/>
                     <br/>
@@ -46,7 +58,7 @@ const Nutrition = () => {
                     <br/>
 
 
-                </Paper>
+                {/* </Paper> */}
             </Container></>
     );
 };
