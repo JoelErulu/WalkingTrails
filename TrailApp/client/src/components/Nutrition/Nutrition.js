4a9790f9112a1c77fd9dc42fe1d4b83f581ec28a
Daniel Valencia <dvalencia2@ggc.edu>
2023-04-25 19:36:37 -0400

    nutrition create added

--- TrailApp/client/src/components/Nutrition/Nutrition.js
+++ TrailApp/client/src/components/Nutrition/Nutrition.js
@@ -1,14 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Grid, Typography, Container, Button, Paper, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import { AppBar, Grid, Typography, Container, Button, Paper, FormControl, InputLabel, Select, MenuItem, TextField, } from '@material-ui/core';
 import { useDispatch, useSelector} from 'react-redux';
 import useStyles from './styles.js';
-import { createMarker, getMarkers, updateMarker, deleteMarker} from '../../actions/markers.js';
+import { createNutrition, getNutrition, updateNutrition, deleteNutrition} from '../../actions/nutritions.js';
 import FileBase from 'react-file-base64';
 
 const Nutrition = () => {
 
     const classes = useStyles();
+
+    const initialState = {name: '',};
+
+    //gets markers from store
+    const {nutrition, isLoading} = useSelector((state) => state.nutrition);
+
+    const [nutritionFormData, setNutritionFormData] = useState(initialState);
+    const [center, setCenter] = useState('');
+    const [selectedNutrition, setSelectedNutrition] = useState(null);
+    const [open, setOpen] = useState(false);
+    const dispatch = useDispatch();
+
+    //get markers from db
+    useEffect(() => {
+        dispatch(getNutrition());
+    }, [dispatch])
+
+
+    //submit form to create marker
+    const handleSubmit = (e) => {
+        // e.preventDefault();
+
+        dispatch(createNutrition(nutritionFormData));
+
+        // if (selectedMarker.key) {
+        //     dispatch(updateNutrition(selectedMarker._id, markerFormData));
+        // }
+        // clear();
+        
+    
+    };
+
+    // const handleDelete = (e) => {
+    //     if (selectedMarker.key) {
+    //         dispatch(deleteMarker(selectedMarker.key))
+    //         clear();
+    //     }
+    // };
+
+    const clear = () => {
+        setSelectedNutrition(null)
+        setNutritionFormData(initialState);
+
+    };
+
     
     return (
         <>
@@ -16,23 +61,13 @@ const Nutrition = () => {
         <Container component="main" maxWidth="xl">
         
 
-                {/* <Paper className={classes.paper} elevation={3}>
+                <Paper className={classes.paper} elevation={3}>
                 <form onSubmit={handleSubmit}>
-                <TextField name='name' variant="outlined" label="Name" margin="normal" value={markerFormData.name}
-                onChange={(e) => setMarkerFormData({...markerFormData, name: e.target.value})}></TextField>
+                <TextField name='name' variant="outlined" label="Name" margin="normal" value={nutritionFormData.name}
+                onChange={(e) => setNutritionFormData({...nutritionFormData, name: e.target.value})}></TextField>
                 <br/>
-                <TextField name='exersice' variant="outlined" label="Exercise" margin="normal" value={markerFormData.exercise}
-                onChange={(e) => setMarkerFormData({...markerFormData, exercise: e.target.value})}></TextField>
-                <br/>
-                <Collapse>
-                <TextField name='lat' variant="outlined" label="Latitude" value = {markerFormData.lat} InputLabelProps={{ shrink: true }} margin="normal"></TextField>
-                <br/>
-                <TextField name='lng' variant="outlined" label="Longitude" value = {markerFormData.lng} InputLabelProps={{ shrink: true }} margin="normal"></TextField>
-                </Collapse>
-                <br/>
-                <div><FileBase type="file" multiple={false} onDone={({ base64 }) => setMarkerFormData({ ...markerFormData, img: base64 })}/></div>
                 <Button type='submit' color="primary" variant="contained">Create</Button>
-                </form> */}
+                </form>
                     <br/>
                     <br/>
                     <br/>
@@ -58,7 +93,7 @@ const Nutrition = () => {
                     <br/>
 
 
-                {/* </Paper> */}
+                </Paper>
             </Container></>
     );
 };
