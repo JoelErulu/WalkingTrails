1b43013333f223be6e25477275d13bf0d9d1cfcc
KietLy <kito_ken123@outlook.com>
2024-04-14 18:37:30 -0400

    Todo.md and Lisence.md added to docs-Spr2024
    Also made changes to the footer section.

--- client/src/components/Navbar/Footer.js
+++ client/src/components/Navbar/Footer.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '10vh',
+    },
+    content: {
+        flexGrow: 1,
+    },
     appBar: {
         borderRadius: 15,
         margin: '30px 0',
         display: 'flex',
-      },
+        width: '100%',
+    },
     footerText: {
         flexGrow: 1,
         textAlign: 'center',
@@ -17,13 +26,18 @@ const Footer = () => {
     const classes = useStyles();
 
     return (
-        <AppBar position="static" color="inherit" className={classes.appBar}>
-            <Toolbar>
-                <Typography variant="body1" className={classes.footerText}>
-                    &copy; Walking Trails
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <div className={classes.root}>
+            <div className={classes.content}>
+                {/* All other page components go here, this div pushes the footer down */}
+            </div>
+            <AppBar position="static" color="inherit" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="body1" className={classes.footerText}>
+                        © 2024 Walking Trails. All rights reserved.
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 };
 
---
+++ docs-Spr2024/TODO.md
@@ -0,0 +1,7 @@
+1. Have comments and reviews of trails posted on the blog
+2. Offline/True GPS tracking (High difficulty, if you want to do this then start immediately, or you won't have enough time to complete)
+3. Functioning database to store media and marker information
+4. Possibly newsletter functionality as per requested by the clients
+5. Like and dislike function to videos
+6. Rework on admin home page for better visual
+7. Tie video with the right marker instead of hard coding videos on them
