c5693995404ab285b0399216366e35c76e4a39f7
Ginette <awilsonbahun@ggc.edu>
2024-09-05 17:44:28 -0400

    Moved the menu bar to the left and the text to the right side

--- TrailApp/client/src/components/Navbar/Navbar.js
+++ TrailApp/client/src/components/Navbar/Navbar.js
@@ -1,133 +1,115 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { AppBar, Avatar, Button, Toolbar, Typography, Menu, MenuItem } from '@material-ui/core';
-import { useDispatch, useSelector } from 'react-redux';
-import { googleLogout } from '@react-oauth/google';
-import { logout } from '../../api/index.js';
+import { AppBar, Button, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
-import GGC from '../../images/GGClogo.jpg';
 
 const Navbar = () => {
     const classes = useStyles();
-    const [anchorEl, setAnchorEl] = useState(null);// For Menu anchor
+    const [anchorEl, setAnchorEl] = useState(null);
     const profile = JSON.parse(localStorage.getItem('profile'));
-    const [ user, setUser ] = useState(profile?.payload);
-    const handleMenuClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
+    const [user, setUser] = useState(profile?.payload);
+    const [userRole, setUserRole] = useState('');
+    
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const location = useLocation();
-    const [userRole, setUserRole] = useState('');
-    const pages = ['Products', 'Pricing', 'Blog'];
+
+    const handleMenuClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
     const logout = () => {
         dispatch({ type: 'LOGOUT' });
-        
         setUser(null);
-
         navigate('/#');
     };
 
     useEffect(() => {
-        // const token = user?.token;
-
         const currentUser = JSON.parse(localStorage.getItem("profile"));
         const currentUserRole = currentUser?.result?.role;
         setUserRole(currentUserRole);
-
-        setUser(JSON.parse(localStorage.getItem('profile')))
+        setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
 
-
     return (
-        <AppBar className={classes.appBar} position="static" color="inherit">
-            <div className={classes.brandContainer} >
-                <Typography component={Link} to={ user ? "/home" : "/" } className={classes.heading} variant="h2" align="center">Walking Trails</Typography>
-                {/* <Link class = "align-right btn btn-primary" component={Link} to="/auth" variant="contained" color="primary" className={classes.submit}>Sign In</Link>
-               {/* <Link to={ user ? "/home" : "/" }>
-                <img className={classes.image} src={GGC} alt="GGC Bear" height="60"/>
-                </Link> */}
-            </div>
-            <div>
-                {userRole==="admin"?<Link to="/admin"><Button variant="contained" color="primary">Admin</Button></Link>:''}
-            </div>
-            {/* {user ? (
-                <Toolbar className={classes.toolbar}>
+        <AppBar className={classes.appBar} position="static">
+            <Toolbar className={classes.toolbar}>
                 
-                    <div className={classes.profile}> */}
-                        {/* <Avatar className={classes.purple} alt={user.name} src={user.picture}>{user.name}</Avatar> */}
-                        {/* <Typography className={classes.userName} variant="h6">Welcome, {user.name}!</Typography> */}
-                        {/* <Typography className={classes.userName} variant="h6">Welcome!</Typography> */}
-                        {/* <Button variant="contained" className={classes.logout} color="primary" onClick={logout}>Logout</Button> */}
-                    {/* </div>
-                </Toolbar>
-            ) : (
-                <MenuItem className={classes.drop} variant="h2" align="right">menu</MenuItem>
-            )} */}
-            <Button Button variant="contained" className={classes.logout} color="primary"
-                aria-controls="user-menu"
-                aria-haspopup="true"
-                onClick={handleMenuClick}
-            >
-            menu
-            </Button>
-            <Menu
-                id="user-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-                >
-                <MenuItem onClick={handleMenuClose} component={Link} to="/">Home</MenuItem>
-                {/* <MenuItem onClick={handleMenuClose} component={Link} to="/#workout">Personalized Workout</MenuItem>
-                <MenuItem onClick={handleMenuClose} component={Link} to="/#community">Community Partners</MenuItem>
-                <MenuItem onClick={handleMenuClose} component={Link} to="/#questions">F.A.Q</MenuItem>
-                <MenuItem onClick={handleMenuClose} component={Link} to="/#instructors">Our Team</MenuItem> */}
-                {/* need to fix links so they work even when you are not on the landing page */}
-                <MenuItem
-                    onClick={() => {
-                        handleMenuClose();
-                        document.getElementById('workout').scrollIntoView({ behavior: 'smooth' });
-                    }}
-                >
-                    Personalized Workout
-                </MenuItem>
-                <MenuItem
-                    onClick={() => {
-                        handleMenuClose();
-                        document.getElementById('community').scrollIntoView({ behavior: 'smooth' });
-                    }}
-                >
-                    Community Partners
-                </MenuItem>
-                <MenuItem
-                    onClick={() => {
-                        handleMenuClose();
-                        document.getElementById('questions').scrollIntoView({ behavior: 'smooth' });
-                    }}
-                >
-                    F.A.Q
-                </MenuItem>
-                <MenuItem
-                    onClick={() => {
-                        handleMenuClose();
-                        document.getElementById('instructors').scrollIntoView({ behavior: 'smooth' });
-                    }}
-                >
-                    Our Team
-                </MenuItem>
+                {/* Move Menu to the Left */}
+                <div className={classes.menuContainer}>
+                    <Button
+                        variant="contained"
+                        className={classes.menuButton}
+                        color="primary"
+                        aria-controls="user-menu"
+                        aria-haspopup="true"
+                        onClick={handleMenuClick}
+                    >
+                        Menu
+                    </Button>
+                    <Menu
+                        id="user-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                        classes={{ paper: classes.menu }}
+                    >
+                        <MenuItem onClick={handleMenuClose} component={Link} to="/">Home</MenuItem>
+                        <MenuItem
+                            onClick={() => {
+                                handleMenuClose();
+                                document.getElementById('workout').scrollIntoView({ behavior: 'smooth' });
+                            }}
+                        >
+                            Personalized Workout
+                        </MenuItem>
+                        <MenuItem
+                            onClick={() => {
+                                handleMenuClose();
+                                document.getElementById('community').scrollIntoView({ behavior: 'smooth' });
+                            }}
+                        >
+                            Community Partners
+                        </MenuItem>
+                        <MenuItem
+                            onClick={() => {
+                                handleMenuClose();
+                                document.getElementById('questions').scrollIntoView({ behavior: 'smooth' });
+                            }}
+                        >
+                            F.A.Q
+                        </MenuItem>
+                        <MenuItem
+                            onClick={() => {
+                                handleMenuClose();
+                                document.getElementById('instructors').scrollIntoView({ behavior: 'smooth' });
+                            }}
+                        >
+                            Our Team
+                        </MenuItem>
+                        {user ? (
+                            <MenuItem onClick={logout}>Logout</MenuItem>
+                        ) : (
+                            <MenuItem onClick={handleMenuClose} component={Link} to="/auth">Sign in / Sign up</MenuItem>
+                        )}
+                    </Menu>
+                </div>
 
-                {user ? (
-                <MenuItem onClick={logout}>Logout</MenuItem>   
-                    ) : (
-                        <MenuItem onClick={handleMenuClose} component={Link} to="/auth">Sign in / Sign up</MenuItem>
-                )}
-            </Menu>
+                {/* Move Title to the Right */}
+                <div className={classes.textContainer}>
+                    <Typography component={Link} to={user ? "/home" : "/"} className={classes.heading} variant="h1" align="right">
+                        WALKING TRAILS
+                    </Typography>
+                </div>
+            </Toolbar>
         </AppBar>
-   );
+    );
 };
 
 export default Navbar;
