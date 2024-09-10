3c796ad5867eb37ba123d461c59e9ca165413166
Joel <135941675+JoelErulu@users.noreply.github.com>
2024-09-10 16:51:05 -0400

    Delete TrailApp/client/src/components/components directory
    Acidentally 2 component files

--- TrailApp/client/src/components/components/Blog/styles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export default makeStyles((theme) => ({
-  gridContainer: {
-    [theme.breakpoints.down('xs')]: {
-      flexDirection: 'column-reverse',
-    },
-  },
-}));
--- TrailApp/client/src/components/components/__tests__/Home.test.js
+++
@@ -1,7 +0,0 @@
-//import Home from '../Home/Home';
-import {render, screen} from '@testing-library/jest-dom';
-test("Testing homepage ", () =>{
-
-    //go
-    expect(true).toBe(false);
-})
