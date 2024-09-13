410195def9596b493f555eeb11499800c07219ea
Ginette <awilsonbahun@ggc.edu>
2024-09-13 01:39:22 -0400

    Added climate page

---
+++ TrailApp/client/src/components/Climate.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import '../../interfaceSettings.css'; // Import the CSS file
+import GwinnettLogo from './gwinnett.png';  // Import the image properly
+
+const Climate = () => {
+    return (
+        <section id="climate" className="container mt-5">
+            <h1>Climate</h1>
+            <p>Welcome to the Climate section! Learn about the effects of climate on fitness and how to adapt your workout to different weather conditions.</p>
+            {/* Add more content as needed */}
+        </section>
+    );
+};
+
+export default Climate;
