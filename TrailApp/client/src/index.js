c8ca209ddd73ec93026c68bf0de89c44639fe5ea
Ginette <awilsonbahun@ggc.edu>
2024-09-12 23:29:55 -0400

    imported bootstrap to index.js

--- TrailApp/client/src/index.js
+++ TrailApp/client/src/index.js
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import reducers from './reducers'
 
