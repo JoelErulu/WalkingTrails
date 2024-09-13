4260f351e0c1bd8dd49832df112c1e3b2c2f32f4
Ginette <awilsonbahun@ggc.edu>
2024-09-13 01:25:21 -0400

    added nutrition page

---
+++ TrailApp/client/src/components/Nutrition.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import '../../interfaceSettings.css'; // Import the CSS file
+import GwinnettLogo from './gwinnett.png';  // Import the image properly
+
+<span>Welcome to the Nutrition Page</span>
