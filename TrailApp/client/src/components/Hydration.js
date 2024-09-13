7bb5cf29dcbf80e4b97269495440fb9751154498
Ginette <awilsonbahun@ggc.edu>
2024-09-13 01:35:26 -0400

    added hydration page

---
+++ TrailApp/client/src/components/Hydration.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import '../../interfaceSettings.css'; // Import the CSS file
+import GwinnettLogo from './gwinnett.png';  // Import the image properly
+
+import React from 'react';
+
+const Hydration = () => {
+    return (
+        <section id="hydration" className="container mt-5">
+            <h1>Hydration</h1>
+            <p>Welcome to the Hydration section! Discover the importance of staying hydrated and tips to improve your water intake.</p>
+            {/* Add more content as needed */}
+        </section>
+    );
+};
+
+export default Hydration;
