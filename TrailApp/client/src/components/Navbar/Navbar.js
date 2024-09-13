8dc240f103c0038749fdbb3ff455522e6d75a1ff
Ginette <awilsonbahun@ggc.edu>
2024-09-12 23:24:35 -0400

    Added the gwinnet logo to the navbar

--- TrailApp/client/src/components/Navbar/Navbar.js
+++ TrailApp/client/src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { AppBar, Button, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import '../../interfaceSettings.css'; // Import the CSS file
+import GwinnettLogo from './gwinnett.png';  // Import the image properly
 
 const Navbar = () => {
     const [anchorEl, setAnchorEl] = useState(null);
     const profile = JSON.parse(localStorage.getItem('profile'));
     const [user, setUser] = useState(profile?.payload);
     const [userRole, setUserRole] = useState('');
-    
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const location = useLocation();
@@ -36,76 +36,57 @@ const Navbar = () => {
     }, [location]);
 
     return (
-        <AppBar className="appBar" position="static">
-            <Toolbar className="toolbar">
-                {/* Move Title to the Left */}
-                <div className="textContainer">
-                    <Typography component={Link} to={user ? "/home" : "/"} className="heading" variant="h1">
-                        WALKING TRAILS
-                    </Typography>
-                </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-white">
+            <div className="container-fluid">
+                {/* Logo and "WALKING TRAILS" */}
+                <Link className="navbar-brand d-flex align-items-center" to={user ? "/home" : "/"}>
+                    <img src={GwinnettLogo} alt="Gwinnett Logo" className="logo me-2" />
+                    <span className="heading">WALKING TRAILS</span>
+                </Link>
 
-                {/* Move Menu to the Right */}
-                <div className="menuContainer">
-                    <Button
-                        variant="contained"
-                        className="menuButton"
-                        aria-controls="user-menu"
-                        aria-haspopup="true"
-                        onClick={handleMenuClick}
-                    >
-                        Menu
-                    </Button>
-                    <Menu
-                        id="user-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleMenuClose}
-                        className="menu"
-                    >
-                        <MenuItem onClick={handleMenuClose} component={Link} to="/">Home</MenuItem>
-                        <MenuItem
-                            onClick={() => {
-                                handleMenuClose();
-                                document.getElementById('workout').scrollIntoView({ behavior: 'smooth' });
-                            }}
-                        >
-                            Personalized Workout
-                        </MenuItem>
-                        <MenuItem
-                            onClick={() => {
-                                handleMenuClose();
-                                document.getElementById('community').scrollIntoView({ behavior: 'smooth' });
-                            }}
-                        >
-                            Community Partners
-                        </MenuItem>
-                        <MenuItem
-                            onClick={() => {
-                                handleMenuClose();
-                                document.getElementById('questions').scrollIntoView({ behavior: 'smooth' });
-                            }}
-                        >
-                            F.A.Q
-                        </MenuItem>
-                        <MenuItem
-                            onClick={() => {
-                                handleMenuClose();
-                                document.getElementById('instructors').scrollIntoView({ behavior: 'smooth' });
-                            }}
-                        >
-                            Our Team
-                        </MenuItem>
+                {/* Menu Button */}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#workout">Personalized Workout</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#community">Hydration</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#questions">Nutrition</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#instructors">Climate</a>
+                        </li>
                         {user ? (
-                            <MenuItem onClick={logout}>Logout</MenuItem>
+                            <li className="nav-item">
+                                <button className="btn btn-outline-danger" onClick={logout}>Logout</button>
+                            </li>
                         ) : (
-                            <MenuItem onClick={handleMenuClose} component={Link} to="/auth">Sign in / Sign up</MenuItem>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/auth">Sign in / Sign up</Link>
+                            </li>
                         )}
-                    </Menu>
+                    </ul>
                 </div>
-            </Toolbar>
-        </AppBar>
+            </div>
+        </nav>
     );
 };
 
